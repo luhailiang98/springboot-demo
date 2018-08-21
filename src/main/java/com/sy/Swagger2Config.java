f06bdd5e21f6d55b8a9392c8fc5f9d958f81235f
luhailiang <1176239106@qq.com>
2018-08-21 14:11:22 +0800

    整合Swagger2

---
+++ src/main/java/com/sy/Swagger2Config.java
@@ -0,0 +1,56 @@
+package com.sy;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @Auther: luhailiang
+ * @Date: 2018/8/21 13:24
+ * @Description:
+ */
+@EnableSwagger2
+@Configuration
+public class Swagger2Config extends WebMvcConfigurationSupport {
+
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("swagger-ui.html")
+        .addResourceLocations("classpath:/META-INF/resources/").setCachePeriod(0);
+  }
+
+  @Bean
+  public Docket createRestApi() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .apiInfo(apiInfo())
+        .select()
+        //为当前包路径
+        .apis(RequestHandlerSelectors.basePackage("com.sy.controller"))
+        .paths(PathSelectors.any())
+        .build();
+  }
+
+  //构建 api文档的详细信息函数
+  private ApiInfo apiInfo() {
+    return new ApiInfoBuilder()
+        //页面标题
+        .title("Spring Boot 测试使用 Swagger2 构建RESTful API")
+        //创建人
+        .contact(new Contact("肆意。-", "https://hailiang.io", "1176239106@qq.com"))
+        //版本号
+        .version("1.0")
+        //描述
+        .description("API 描述")
+        .build();
+  }
+}
--- src/main/java/com/sy/controller/SysUserController.java
+++ src/main/java/com/sy/controller/SysUserController.java
@@ -3,17 +3,18 @@ package com.sy.controller;
 import com.sy.model.SysUser;
 import com.sy.service.SysUserService;
 import com.sy.utils.JsonData;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Auther: luhailiang
  * @Date: 2018/8/9 13:04
  * @Description:
  */
-@Controller
+@RestController
 @RequestMapping(value = "/user")
 public class SysUserController {
 
@@ -21,10 +22,10 @@ public class SysUserController {
   private SysUserService sysUserService;
 
 
-  @RequestMapping(value = "/saveUser")
-  @ResponseBody
+  @ApiOperation(value = "创建用户", notes = "根据User对象创建用户")
+  @RequestMapping(value = "/saveUser", method = RequestMethod.POST)
   public JsonData saveUser(SysUser user) {
     sysUserService.saveUser(user);
-    return JsonData.success(user,"新增成功");
+    return JsonData.success(user, "新增成功");
   }
 }
